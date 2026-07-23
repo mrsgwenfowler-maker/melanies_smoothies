fbfe4dec57a6158846d8946b6f22a1a41456fb1a
mrsgwenfowler-maker <mrsgwenfowler@gmail.com>
2026-07-23 09:22:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app
 st.title("🥤 Customize your Smoothie! 🥤")
@@ -48,6 +48,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
+
 
 
