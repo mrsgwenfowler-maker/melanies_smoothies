ee4d950551320f61b2e0c4ac8545204c61aa0734
mrsgwenfowler-maker <mrsgwenfowler@gmail.com>
2026-07-23 09:15:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,5 @@ smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
 
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_reponse.json(), use_container_width=True)
